a885d30942d38859a8847b84b5f90febc88c8e36
Robert Lin <robertlin1@gmail.com>
2017-10-31 23:52:41 -0700

    Refactor index to server

--- test/init.js
+++ test/init.js
@@ -4,7 +4,7 @@ const cli = require('heroku-cli-util')         // Load heroku-cli-util helpers
 //const nock = require('nock')                   // Load nock
 cli.raiseErrors = true                         // Fully raise exceptions
 //nock.disableNetConnect()                       // Disable HTTP connections
-global.commands = require('../index').commands // Load plugin commands
+global.commands = require('../server').commands // Load plugin commands
 
 process.env.TZ = 'UTC'                         // Use UTC time always
 //require('mockdate').set(new Date())            // Freeze time (optional)
